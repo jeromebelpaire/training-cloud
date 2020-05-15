a29adf79a3796c74fc168369b5d2d8469a4d0e04
Jerome Belpaire <jerome@agilytic.be>
2020-05-15 23:10:51 +0200

    Basic API test

--- App/app.py
+++ App/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from Scripts.predictFunction import predictAuthor
+import os
+import pickle
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Check currentdir
+    currentDirPath = os.path.dirname(os.path.realpath(__file__))
+
+    # load trained prediction nmodel:
+    with open(os.path.dirname(currentDirPath) + "/Models/bestPerformingModel/bestPerformingModel.pkl", 'rb') as file:
+        predictionModel = pickle.load(file)
+
+    result = predictAuthor(predictionModel=predictionModel,
+                           directText="Cloud is the future")
+    predicted_author = eval(result)["predicted authors"][0]
+    return render_template('index.html', prediction=predicted_author)
 
 
 if __name__ == "__main__":
