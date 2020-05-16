baba2d140ccb89847540d6857c1a986c7f85d43a
Jerome Belpaire <jerome@agilytic.be>
2020-05-16 10:29:13 +0200

    Added submission form

--- App/app.py
+++ App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from Scripts.predictFunction import predictAuthor
 import os
 import pickle
@@ -6,20 +6,32 @@ import pickle
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Check currentdir
-    currentDirPath = os.path.dirname(os.path.realpath(__file__))
+    if request.method == 'POST':
+        # Check currentdir
+        currentDirPath = os.path.dirname(os.path.realpath(__file__))
 
-    # load trained prediction nmodel:
-    with open(os.path.dirname(currentDirPath) + "/Models/bestPerformingModel/bestPerformingModel.pkl", 'rb') as file:
-        predictionModel = pickle.load(file)
+        input_text = request.form.get('input_text')
 
-    result = predictAuthor(predictionModel=predictionModel,
-                           directText="Cloud is the future")
-    predicted_author = eval(result)["predicted authors"][0]
-    return render_template('index.html', prediction=predicted_author)
+        if len(input_text) > 0:
+            # load trained prediction nmodel:
+            with open(os.path.dirname(currentDirPath) + "/Models/bestPerformingModel/bestPerformingModel.pkl", 'rb') as file:
+                predictionModel = pickle.load(file)
+
+            result = predictAuthor(predictionModel=predictionModel,
+                                   directText=input_text)
+            predicted_author = eval(result)["predicted authors"][0]
+        else:
+            predicted_author = 'Could not predict empty text'
+
+    else:
+        input_text = ''
+        predicted_author = ''
+    return render_template('index.html',
+                           input_text=input_text,
+                           prediction=predicted_author)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5555)
